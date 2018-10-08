ffba9de6df41ad455e267ed567765105e0e27544
Augustin Ionascu <ionutionascu27@gmail.com>
2018-10-08 21:37:08 +0000

    number two generator added

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ### first and the easiest: I created the board (roughly) - 4 x 4 cells
 ### each cell got attributes for line and column numbers
+### number 2 generator function created:
+##### highlighted tile div created with Math.random()
+##### number two div applied over the highlighted tile, with .position() function
--- engine.js
+++ engine.js
@@ -12,5 +12,20 @@ $(document).ready(function create_board(){
         $(this).children().attr("line",$(this).attr("line"));
         $(this).removeAttr("line");
     });
+    generate_random_two();
+    generate_random_two();
     $('body').append('<h2 id = "scorediv">Your Highest Score:</h2>');
 });
+
+function generate_random_two(){
+    var rowLength = $(".empty-tile").length;
+    var random = Math.floor(Math.random() * rowLength);
+    var selectTile = $(".empty-tile").eq(random);
+    $(selectTile).removeClass("empty-tile");
+    $(selectTile).addClass("random-tile");
+    var tilePosition = $(selectTile).position();
+    console.log(tilePosition);
+     $('#board-table').append(`<div id = "new-tile" class = "new-tile" line = ${(selectTile).attr("line")} column = ${(selectTile).attr("column")}>2</div>`);
+     $('#new-tile').css({top:(tilePosition.top+3)+'px',left:(tilePosition.left+3)+'px', width:($(selectTile).width())+'px', height:($(selectTile).height())+'px'});
+     $('#new-tile').attr('id',"");
+}

e81a585fd7a5cdb3bfb51fe76816a69bb0ad9fd4
Augustin Ionascu <ionutionascu27@gmail.com>
2018-10-14 11:19:33 +0000

    created keypress event function with the general logic of the game, movement and number multiplication conditions and functions created for left key press code

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 ### number 2 generator function created:
 ##### highlighted tile div created with Math.random()
 ##### number two div applied over the highlighted tile, with .position() function
+### another two global variables declared, one to determine if a next move is possible or the game is over, the other one to check if any cell has moved (a new random "two" shall be generated if true)
+### next step: I created a keydown event function (the plan is to assign the game controls to the directional keys) - switch statement with 4 cases for the directional keycodes
+
+### first case for the left key (case 37):
+
+##### declared the position of the random number cell (row and column) and the destination position(same line and first column - when pressing the left key the movement of the cell shall be columnwise with decrement to the first column)
+##### declared the conditions for moving the cells and numbers multiplication (column number greater than 1; the last random created and the number next to it to be equal)
+##### created animation function with multiplication function as callback
+##### assign the right identifying css classes for the iterated cells (from the current line) and the target cell
+
+
+
--- engine.js
+++ engine.js
@@ -1,12 +1,15 @@
-// generate board with tiles
+var move_possible = false;
+var cell_moved = false;
+
+// generate board with cells
 $(document).ready(function create_board(){
-    $('body').append('<h1 id = "titlediv"><strong>2048 - PLAY NOW!</strong></h1>');
+    $('body').append('<h1 id = "titlediv"><strong>2048 DE URSI - JOACA ACUM!</strong></h1>');
     $('body').append('<div id = "board-table" class = "col-12 col-sm-8 col-md-6 col-lg-4"></div>');
         for (var i=0; i<4; i++){
             $('#board-table').append(`<div class = "wrapper d-flex" line=${i+1}></div>`);
         }
         for (var j=0; j<4; j++){
-            $(".wrapper").append(`<div class = "col-3 empty-tile" column=${j+1}></div>`);
+            $(".wrapper").append(`<div id = "global-cell" class = "col-3 global-cell empty-cell" column=${j+1}></div>`);
         }
     $(".wrapper").each(function(){
         $(this).children().attr("line",$(this).attr("line"));
@@ -14,18 +17,73 @@ $(document).ready(function create_board(){
     });
     generate_random_two();
     generate_random_two();
-    $('body').append('<h2 id = "scorediv">Your Highest Score:</h2>');
+    $('body').append('<h2 id = "scorediv">Your Score:</h2>');
 });
 
 function generate_random_two(){
-    var rowLength = $(".empty-tile").length;
+    var rowLength = $(".empty-cell").length;
     var random = Math.floor(Math.random() * rowLength);
-    var selectTile = $(".empty-tile").eq(random);
-    $(selectTile).removeClass("empty-tile");
-    $(selectTile).addClass("random-tile");
-    var tilePosition = $(selectTile).position();
-    console.log(tilePosition);
-     $('#board-table').append(`<div id = "new-tile" class = "new-tile" line = ${(selectTile).attr("line")} column = ${(selectTile).attr("column")}>2</div>`);
-     $('#new-tile').css({top:(tilePosition.top+3)+'px',left:(tilePosition.left+3)+'px', width:($(selectTile).width())+'px', height:($(selectTile).height())+'px'});
-     $('#new-tile').attr('id',"");
+    var selectTile = $(".empty-cell").eq(random);
+    $(selectTile).removeClass("empty-cell");
+    $(selectTile).addClass("full-cell");
+    var cellPosition = $(selectTile).position();
+    console.log(cellPosition);
+     $('#board-table').append(`<div id = "last-number" class = "number-cell" line = ${(selectTile).attr("line")} column = ${(selectTile).attr("column")}>2</div>`);
+     $('#last-number').css({top:(cellPosition.top+3)+'px',left:(cellPosition.left+3)+'px', width:($(selectTile).width())+'px', height:($(selectTile).height())+'px'});
+     $('#last-number').attr('id',"");
+     move_possible = true;
 }
+
+$(document).keydown(function(event){
+    if (move_possible){
+        move_possible=false;
+        cell_moved=false;
+        switch (event.which || event.keyCode) {
+            case 37 :
+            $('.number-cell').each(function(){
+                var current_line = $(this).attr('line');
+                console.log(current_line);
+                var current_column = $(this).attr('column');
+                var target_column = current_column;
+                var similar_data = false;
+                if (current_column>=0){
+                var i;
+                    for (i = current_column-1; i >= 1; i --){
+                        if ($('.global-cell[line='+current_line+'][column='+i+']').hasClass('full-cell')){
+                            if ($(this).html() == $('.number-cell[line='+current_line+'][column='+i+']').html()){
+                                similar_data = true;
+                                target_column = i;
+                            }
+                            break;
+                        }else{
+                            target_column = i;
+                            }
+                    }
+                    console.log('final'+target_column);
+                    if (current_column!=target_column){
+                        cell_moved = true;
+                    }
+                    $(this).animate({ left: '-=' +(document.getElementById('global-cell').offsetWidth*(current_column-target_column))},300,function(){
+                        if (similar_data){
+                            $('.number-cell[line='+current_line+'][column='+target_column+']').html(parseInt($(this).html()*2));
+                            $(this).remove();
+                        }
+                    });
+                    $('.global-cell[line='+current_line+'][column='+current_column+']').removeClass('full-cell').addClass('empty-cell');
+					$(this).attr('column',target_column);
+					$('.global-cell[line='+current_line+'][column='+target_column+']').removeClass('empty-cell').addClass('full-cell');
+                }
+            });
+            break;
+        }
+        if (cell_moved){
+                generate_random_two();
+            }else{
+                move_possible = true;
+            }
+    }else{
+	document.getElementById('scorediv').innerHTML = "GAME OVER!<br>Your highest score is:";
+	}
+});
+    
+    
